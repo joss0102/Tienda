ed7cd9655e3a9e8a6dcf94df96b070d7979bf771
joss.0102 <joserosellbonavina@gmail.com>
2025-02-24 23:38:18 +0100

    commit nuevo

--- src/app/cliente/tienda/tienda.component.ts
+++ src/app/cliente/tienda/tienda.component.ts
@@ -1,38 +1,59 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CarritoService } from '../../services/carrito.service';
-import { CompraComponent } from "../tabs/compra/compra.component";
-import { CarritoComponent } from "../carrito/carrito.component";
 import { LibroService } from '../../services/libro-service.service';
 import { Libro } from '../../services/models/libro.model';
-
+import { CompraComponent } from '../tabs/compra/compra.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tienda',
-  imports: [CommonModule, CompraComponent, CarritoComponent],
-  standalone: true,
+  imports:[CompraComponent, NgFor],
   templateUrl: './tienda.component.html',
   styleUrls: ['./tienda.component.scss']
 })
+
 export class TiendaComponent implements OnInit {
   libros: Libro[] = [];
-  isVisible = false;
-  libroSeleccionado: Libro | null = null;  // Almacena el libro seleccionado
+  librosPorPagina: number = 6;
+  paginaActual: number = 1;
+  totalPaginas: number = 0;
+  libroSeleccionado: Libro | null = null;
 
   constructor(
     private carritoService: CarritoService,
-    private libroService: LibroService // Inyectamos el servicio de libros
+    private libroService: LibroService
   ) {}
 
   ngOnInit(): void {
-    // Obtener los libros a través del servicio
     this.libros = this.libroService.getLibros();
+    this.totalPaginas = Math.ceil(this.libros.length / this.librosPorPagina); // Calcula el número total de páginas
+    this.cargarLibros();
+  }
+
+  cargarLibros() {
+    const inicio = (this.paginaActual - 1) * this.librosPorPagina;
+    const fin = inicio + this.librosPorPagina;
+    this.libros = this.libroService.getLibros().slice(inicio, fin);
+  }
+
+  // Cambiar a la página siguiente
+  siguientePagina() {
+    if (this.paginaActual < this.totalPaginas) {
+      this.paginaActual++;
+      this.cargarLibros();
+    }
+  }
+
+  // Cambiar a la página anterior
+  paginaAnterior() {
+    if (this.paginaActual > 1) {
+      this.paginaActual--;
+      this.cargarLibros();
+    }
   }
 
   agregarAlCarrito(libro: Libro) {
     console.log("Libro agregado:", libro);
     this.carritoService.agregarLibro(libro);
-    this.isVisible = true;
-    this.libroSeleccionado = libro;
   }
 }
